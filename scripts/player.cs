6c26f472f6271ff6a298023ddf493f596cc1fbdd
jfjvhbhfnbujd <qwertyisweird123@gmail.com>
2022-12-02 13:54:58 -0800

    stuffs

--- scripts/player.cs
+++ scripts/player.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour
@@ -10,7 +7,7 @@ public class player : MonoBehaviour
     private float horizontalInput;
     public float moveSpeed;
 
-   
+
     public GameObject respawn; //respawn
     public bool HasPowerUp; //powerup
     public GameObject projectileSpawnPoint; //bullet spawn
@@ -18,12 +15,12 @@ public class player : MonoBehaviour
     public ParticleSystem respawnParticles; //particles
     public float shootTime; //shoot time
     public bool lifes = false;
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -32,29 +29,31 @@ public class player : MonoBehaviour
         //move
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.left * Time.deltaTime * moveSpeed * horizontalInput);
-        if(lifes == true)
+        if (lifes == true)
         {
             Instantiate(projectilePrefab, projectileSpawnPoint.transform.position, projectileSpawnPoint.transform.rotation);
         }
     }
 
-    
+
 
     private void OnTriggerEnter(Collider other)
     {
-        
-       
+
+
         if (other.gameObject.CompareTag("Bullet") && HasPowerUp == false)
-            {
-                transform.position = respawn.transform.position;
-                 respawnParticles.Play();
-            lifes = true; 
-            }
+        {
+            transform.position = respawn.transform.position;
+            respawnParticles.Play();
+            lifes = true;
+        }
         if (other.gameObject.CompareTag("PowerUp"))
         {
             Destroy(other.gameObject);
-            HasPowerUp = true;
-            
+            HasPowerUp = true;      
+
+              
+
         }
-      }
+    }
 }
